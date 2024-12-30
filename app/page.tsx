b7a744861c7d51f45f7a6dea354bd5a7423e8f92
Mahmoud Ahmed <wa7wa776@gmail.com>
2024-12-30 10:15:15 +0200

    navigations

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts.ts";
 import Image from "next/image";
+import styles from "@/app/ui/home.module.css";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
